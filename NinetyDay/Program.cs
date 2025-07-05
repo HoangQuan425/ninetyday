3e5c41afe617ccf85eb926eaea1cb0121fe83762
QuanHQ <quanqhhq@gmail.com>
2025-07-05 07:26:41 +0700

    first commit

---
+++ NinetyDay/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace NinetyDay
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int n = int.Parse(Console.ReadLine() ?? "0");
+			int[] arr = new int[n];
+		}
+	}
+}
